4e84622cd7357d18c7202ad5a7f06244e9d28aa8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-29 20:45:14 +0200

    Log to file LoadLibraryExW calls

--- vac-hooks/Hooks.c
+++ vac-hooks/Hooks.c
@@ -1,9 +1,23 @@
+#include <stdio.h>
+
 #include "Hooks.h"
 #include "Utils.h"
+
+#define LOG_FILENAME "vac-hooks.txt"
+#define LOG_BUFFER_SIZE 500
 
 HMODULE WINAPI Hooks_LoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags)
 {
     HMODULE result = LoadLibraryExW(lpLibFileName, hFile, dwFlags);
+    FILE* out;
+
+    if (fopen_s(&out, LOG_FILENAME, "a")) {
+        CHAR buf[LOG_BUFFER_SIZE];
+        sprintf_s(buf, sizeof(buf), "LoadLibraryExW(lpLibFileName: %ws, hFile: %p, dwFlags: %d)\n", lpLibFileName, hFile, dwFlags);
+        fprintf(out, buf);
+        fclose(out);
+    }
+
     Utils_hookImport(lpLibFileName, "kernel32.dll", "GetProcAddress", Hooks_GetProcAddress);
     return result;
 }
