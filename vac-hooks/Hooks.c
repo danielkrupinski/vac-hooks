b031b35632a23231024df64865f3a148c3b8bbdc
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-01 16:31:47 +0200

    Update Hooks.h & Hooks.c

--- vac-hooks/Hooks.c
+++ vac-hooks/Hooks.c
@@ -5,20 +5,11 @@
 #include "Hooks.h"
 #include "Utils.h"
 
-#define LOG_FILENAME "vac-hooks.txt"
-#define LOG_BUFFER_SIZE 500
-
 HMODULE WINAPI Hooks_LoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags)
 {
     HMODULE result = LoadLibraryExW(lpLibFileName, hFile, dwFlags);
-    FILE* out;
 
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "LoadLibraryExW(lpLibFileName: %ws, hFile: %p, dwFlags: %d) -> HMODULE: %p\n", lpLibFileName, hFile, dwFlags, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("LoadLibraryExW(lpLibFileName: %ws, hFile: %p, dwFlags: %d) -> HMODULE: %p\n", lpLibFileName, hFile, dwFlags, result);
 
     Utils_hookImport(lpLibFileName, "kernel32.dll", "GetProcAddress", Hooks_GetProcAddress);
     return result;
@@ -28,14 +19,7 @@ FARPROC WINAPI Hooks_GetProcAddress(HMODULE hModule, LPCSTR lpProcName)
 {
     FARPROC result = GetProcAddress(hModule, lpProcName);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetProcAddress(hModule: %p, lpProcName: %s) -> FARPROC: %p \n", hModule, lpProcName, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetProcAddress(hModule: %p, lpProcName: %s) -> FARPROC: %p \n", hModule, lpProcName, result);
 
     if (!strcmp(lpProcName, "GetProcAddress"))
         return (FARPROC)Hooks_GetProcAddress;
@@ -57,6 +41,10 @@ FARPROC WINAPI Hooks_GetProcAddress(HMODULE hModule, LPCSTR lpProcName)
         return (FARPROC)Hooks_GetModuleFileNameExA;
     else if (!strcmp(lpProcName, "GetModuleFileNameExW"))
         return (FARPROC)Hooks_GetModuleFileNameExW;
+    else if (!strcmp(lpProcName, "GetComputerNameExW"))
+        return (FARPROC)Hooks_GetComputerNameExW;
+    else if (!strcmp(lpProcName, "CreateRemoteThread"))
+        return (FARPROC)Hooks_CreateRemoteThread;
 
     return result;
 }
@@ -64,14 +52,8 @@ FARPROC WINAPI Hooks_GetProcAddress(HMODULE hModule, LPCSTR lpProcName)
 HANDLE WINAPI Hooks_OpenProcess(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId)
 {
     HANDLE result = OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId);
-    FILE* out;
 
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "OpenProcess(dwDesiredAccess: %d, bInheritHandle: %d, dwProcessId: %d) -> HANDLE: %p\n", dwDesiredAccess, bInheritHandle, dwProcessId, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("OpenProcess(dwDesiredAccess: %d, bInheritHandle: %d, dwProcessId: %d) -> HANDLE: %p\n", dwDesiredAccess, bInheritHandle, dwProcessId, result);
 
     return result;
 }
@@ -80,14 +62,7 @@ DWORD WINAPI Hooks_GetProcessImageFileNameA(HANDLE hProcess, LPSTR lpImageFileNa
 {
     DWORD result = GetProcessImageFileNameA(hProcess, lpImageFileName, nSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetProcessImageFileNameA(hProcess: %p, lpImageFileName: %s, nSize: %d) -> DWORD: %d\n", hProcess, lpImageFileName, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetProcessImageFileNameA(hProcess: %p, lpImageFileName: %s, nSize: %d) -> DWORD: %d\n", hProcess, lpImageFileName, nSize, result);
 
     return result;
 }
@@ -96,14 +71,7 @@ int WINAPI Hooks_GetWindowTextW(HWND hWnd, LPWSTR lpString, int nMaxCount)
 {
     int result = GetWindowTextW(hWnd, lpString, nMaxCount);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetWindowTextW(hWnd: %p, lpString: %ws, nMaxCount: %d) -> int %d\n", hWnd, lpString, nMaxCount, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetWindowTextW(hWnd: %p, lpString: %ws, nMaxCount: %d) -> int %d\n", hWnd, lpString, nMaxCount, result);
 
     return result;
 }
@@ -112,14 +80,7 @@ BOOL WINAPI Hooks_QueryFullProcessImageNameW(HANDLE hProcess, DWORD dwFlags, LPW
 {
     BOOL result = QueryFullProcessImageNameW(hProcess, dwFlags, lpExeName, lpdwSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "QueryFullProcessImageNameW(hProcess: %p, dwFlags: %d, lpExeName: %ws, lpdwSize: %p) -> BOOL: %d\n", hProcess, dwFlags, lpExeName, lpdwSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("QueryFullProcessImageNameW(hProcess: %p, dwFlags: %d, lpExeName: %ws, lpdwSize: %p) -> BOOL: %d\n", hProcess, dwFlags, lpExeName, lpdwSize, result);
 
     return result;
 }
@@ -128,14 +89,7 @@ DWORD WINAPI Hooks_GetModuleBaseNameA(HANDLE hProcess, HMODULE hModule, LPSTR lp
 {
     DWORD result = GetModuleBaseNameA(hProcess, hModule, lpBaseName, nSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetModuleBaseNameA(hProcess: %p, hModule: %p, lpBaseName: %s, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpBaseName, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetModuleBaseNameA(hProcess: %p, hModule: %p, lpBaseName: %s, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpBaseName, nSize, result);
 
     return result;
 }
@@ -144,14 +98,7 @@ DWORD WINAPI Hooks_GetModuleBaseNameW(HANDLE hProcess, HMODULE hModule, LPWSTR l
 {
     DWORD result = GetModuleBaseNameW(hProcess, hModule, lpBaseName, nSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetModuleBaseNameW(hProcess: %p, hModule: %p, lpBaseName: %ws, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpBaseName, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetModuleBaseNameW(hProcess: %p, hModule: %p, lpBaseName: %ws, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpBaseName, nSize, result);
 
     return result;
 }
@@ -160,14 +107,7 @@ DWORD WINAPI Hooks_GetModuleFileNameA(HMODULE hModule, LPSTR lpFilename, DWORD n
 {
     DWORD result = GetModuleFileNameA(hModule, lpFilename, nSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetModuleFileNameA(hModule: %p, lpFilename: %s, nSize: %d) -> DWORD: %d\n", hModule, lpFilename, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetModuleFileNameA(hModule: %p, lpFilename: %s, nSize: %d) -> DWORD: %d\n", hModule, lpFilename, nSize, result);
 
     return result;
 }
@@ -176,14 +116,7 @@ DWORD WINAPI Hooks_GetModuleFileNameExA(HANDLE hProcess, HMODULE hModule, LPSTR 
 {
     DWORD result = GetModuleFileNameExA(hProcess, hModule, lpFilename, nSize);
 
-    FILE* out;
-
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetModuleFileNameExA(hProcess: %p, hModule: %p, lpFilename: %s, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpFilename, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    Utils_log("GetModuleFileNameExA(hProcess: %p, hModule: %p, lpFilename: %s, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpFilename, nSize, result);
 
     return result;
 }
@@ -192,14 +125,35 @@ DWORD WINAPI Hooks_GetModuleFileNameExW(HANDLE hProcess, HMODULE hModule, LPWSTR
 {
     DWORD result = GetModuleFileNameExW(hProcess, hModule, lpFilename, nSize);
 
-    FILE* out;
+    Utils_log("GetModuleFileNameExW(hProcess: %p, hModule: %p, lpFilename: %ws, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpFilename, nSize, result);
 
-    if (!fopen_s(&out, LOG_FILENAME, "a")) {
-        CHAR buf[LOG_BUFFER_SIZE];
-        sprintf_s(buf, sizeof(buf), "GetModuleFileNameExW(hProcess: %p, hModule: %p, lpFilename: %ws, nSize: %d) -> DWORD: %d\n", hProcess, hModule, lpFilename, nSize, result);
-        fprintf(out, buf);
-        fclose(out);
-    }
+    return result;
+}
+
+BOOL WINAPI Hooks_GetComputerNameExW(COMPUTER_NAME_FORMAT NameType, LPWSTR lpBuffer, LPDWORD nSize)
+{
+    BOOL result = GetComputerNameExW(NameType, lpBuffer, nSize);
+
+    Utils_log("GetComputerNameExW(NameType: %d, lpBuffer: %ws, nSize: %d) -> BOOL: %d\n", NameType, lpBuffer, *nSize, result);
+
+    return result;
+}
+
+HANDLE WINAPI Hooks_CreateRemoteThread(HANDLE hProcess, LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize, LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId)
+{
+    HANDLE result = CreateRemoteThread(hProcess, lpThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId);
+
+    Utils_log("CreateRemoteThread(hProcess: %p, lpThreadAttributes: %p, dwStackSize: %d, lpStartAddress: %p, lpParameter: %p, dwCreationFlags: %d, lpThreadId: %p) -> HANDLE: %p\n", hProcess, lpThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId, result);
+
+    return result;
+}
+
+NTSTATUS NTAPI Hooks_NtOpenProcess(PHANDLE ProcessHandle, ACCESS_MASK DesiredAccess, PVOID ObjectAttributes, PVOID ClientId)
+{
+    NTSTATUS(NTAPI* NtOpenProcess)(PHANDLE, ACCESS_MASK, PVOID, PVOID) = (PVOID)GetProcAddress(GetModuleHandleW(L"ntdll"), "NtOpenProcess");
+    NTSTATUS result = NtOpenProcess(ProcessHandle, DesiredAccess, ObjectAttributes, ClientId);
+
+    Utils_log("NtOpenProcess(ProcessHandle: %p, DesiredAccess: %d, ObjectAttributes: %p, ClientId: %p) -> NTSTATUS: %l\n", ProcessHandle, DesiredAccess, ObjectAttributes, ClientId, result);
 
     return result;
 }
