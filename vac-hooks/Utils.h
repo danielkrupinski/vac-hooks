370683ab550a521331101121c2401ce7fb5ff214
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-01 14:34:41 +0200

    Add UTILS_HASH() macro

--- vac-hooks/Utils.h
+++ vac-hooks/Utils.h
@@ -4,3 +4,11 @@
 
 PVOID Utils_findPattern(PCWSTR, PCSTR, SIZE_T);
 VOID Utils_hookImport(PCWSTR, PCSTR, PCSTR, PVOID);
+
+#define UTILS_HASH_1(s, i, val) (val * 65599u + ((i) < strlen(s) ? s[strlen(s) - 1- (i)] : 0))
+#define UTILS_HASH_4(s, i, val) UTILS_HASH_1(s, i, UTILS_HASH_1(s, i + 1, UTILS_HASH_1(s, i + 2, UTILS_HASH_1(s, i + 3, val))))
+#define UTILS_HASH_16(s, i, val) UTILS_HASH_4(s, i, UTILS_HASH_4(s, i + 4, UTILS_HASH_4(s, i + 8, UTILS_HASH_4(s, i + 12, val))))
+#define UTILS_HASH_64(s, i, val) UTILS_HASH_16(s, i, UTILS_HASH_16(s, i + 16, UTILS_HASH_16(s, i + 32, UTILS_HASH_16(s, i + 48, val))))
+#define UTILS_HASH_256(s, i, val) UTILS_HASH_64(s, i, UTILS_HASH_64(s, i + 64, UTILS_HASH_64(s, i + 128, UTILS_HASH_64(s, i + 192, val))))
+
+#define UTILS_HASH(s) ((UINT)(UTILS_HASH_256(s, 0, 0) ^ UTILS_HASH_256(s, 0, 0) >> 16))
