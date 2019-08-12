c0ed928154bd762de754497017aaef018af9bfcc
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-12 18:48:54 +0200

    Add SAFE_STR macro

--- vac-hooks/Utils.h
+++ vac-hooks/Utils.h
@@ -6,7 +6,7 @@ PVOID Utils_findPattern(PCWSTR, PCSTR, SIZE_T);
 VOID Utils_hookImport(PCWSTR, PCSTR, PCSTR, PVOID);
 VOID Utils_log(PCSTR, ...);
 
-#define UTILS_HASH_1(s, i, val) (val * 65599u + ((i) < strlen(s) ? s[strlen(s) - 1- (i)] : 0))
+#define UTILS_HASH_1(s, i, val) (val * 65599u + ((i) < strlen(s) ? s[strlen(s) - 1 - (i)] : 0))
 #define UTILS_HASH_4(s, i, val) UTILS_HASH_1(s, i, UTILS_HASH_1(s, i + 1, UTILS_HASH_1(s, i + 2, UTILS_HASH_1(s, i + 3, val))))
 #define UTILS_HASH_16(s, i, val) UTILS_HASH_4(s, i, UTILS_HASH_4(s, i + 4, UTILS_HASH_4(s, i + 8, UTILS_HASH_4(s, i + 12, val))))
 #define UTILS_HASH_64(s, i, val) UTILS_HASH_16(s, i, UTILS_HASH_16(s, i + 16, UTILS_HASH_16(s, i + 32, UTILS_HASH_16(s, i + 48, val))))
@@ -15,3 +15,5 @@ VOID Utils_log(PCSTR, ...);
 #define UTILS_HASH(s) ((UINT)(UTILS_HASH_256(s, 0, 0) ^ UTILS_HASH_256(s, 0, 0) >> 16))
 
 UINT Utils_hashRuntime(PCSTR);
+
+#define SAFE_STR(s, fallback) (s ? s : fallback)
